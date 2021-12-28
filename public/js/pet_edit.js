3c4559097a86f0832a5698855fead29c04a9b664
dajan <dajan@lloyds-digital.com>
2021-12-28 15:29:07 +0100

    js finished

--- public/js/pet_edit.js
+++ public/js/pet_edit.js
@@ -1,22 +1,23 @@
 let allImages = document.getElementsByClassName('img-thumbnail')
-let photosToRemove = toArray(document.getElementById('photos_to_remove').value)
+let photosToRemove = Array.from(document.getElementById('photos_to_remove').value)
 
 for(let i = 0; i < allImages.length; i++){
 
     allImages[i].addEventListener('click', (e)=> {
 
         let selectedPhoto = e.currentTarget;
-        let srcPathArray = selectedPhoto.fullSrc.split('/');
-        let src = srcPathArray[srcPathArray-length - 1]
+        let srcPathArray = selectedPhoto.src.split('/');
+        let src = srcPathArray[srcPathArray.length - 1]
 
         if (!photosToRemove.includes(src)){
             selectedPhoto.classList.toggle('opacity');
             photosToRemove.push(src)
+            console.log(photosToRemove)
         }
         else{
 
             selectedPhoto.classList.toggle('opacity');
-            let photosToRemove = photosToRemove.filter(item => item != src)
+            photosToRemove = photosToRemove.filter(item => item != src)
  
         }
 
